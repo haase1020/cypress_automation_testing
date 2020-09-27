78f31757b8fd259d838718bb44a766b758a7130d
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-27 18:38:32 -0400

    cy.document assertion

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,8 +1,9 @@
 /// <reference types="Cypress" />
 
 describe('Test Contact Us form via WebdriverUni', () => {
-  it('Should be able to submit a successful submission via contact us form', () => {
+  it.only('Should be able to submit a successful submission via contact us form', () => {
     cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
     cy.get('[name="first_name"]').type('Mandi');
     cy.get('[name="last_name"]').type('Haase');
     cy.get('[name="email"]').type('mandi@gmail.com');
