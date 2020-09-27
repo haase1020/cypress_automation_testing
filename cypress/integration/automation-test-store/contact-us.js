531c0c684f1c6cafc460bb772857dd4770d8a902
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-27 18:16:58 -0400

    add to tests

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -10,5 +10,9 @@ describe('Test Contact Us form via Automation Test Store', () => {
     cy.get('#ContactUsFrm_email').type('mandi@gmail.com');
     cy.get('#ContactUsFrm_enquiry').type('Hello everyone!');
     cy.get("button[title='Submit']").click();
+    cy.get('.mb40 > :nth-child(3)').should(
+      'have.text',
+      'Your enquiry has been successfully sent to the store owner!'
+    );
   });
 });
