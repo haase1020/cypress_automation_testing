054303cdd44d8f5cb5a702b7a66455b1e8378c83
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-28 15:32:54 -0400

    more validation

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Verifying variables, cypress commands and jquery comands', () => {
-  it.only('navigateding to specific product pages', () => {
+  it('navigateding to specific product pages', () => {
     cy.visit('https://www.automationteststore.com/');
     // //not recommended way to use cypress
     // const makeupLink = cy
@@ -13,8 +13,38 @@ describe('Verifying variables, cypress commands and jquery comands', () => {
     //   .contains('Skincare');
     // skincareLink.click();
 
-    //right way-->
+    //recommended approach
     cy.get("a[href*='product/category&path=']").contains('Makeup').click();
     cy.get("a[href*='product/category&path=']").contains('Skincare').click;
+  });
+
+  it('navigating to specific product pages', () => {
+    cy.visit('https://www.automationteststore.com/');
+    cy.get("a[href*='product/category&path=']").contains('Makeup').click();
+
+    // following code will fail
+    // const header = cy.get('h1 .maintext');
+    // cy.log(header.text());
+
+    //recommended approach
+    cy.get('h1 .maintext').then(($headerText) => {
+      const headerText = $headerText.text();
+      cy.log('found header text: ' + headerText);
+      //assertion
+      expect(headerText).is.eq('Makeup');
+    });
+  });
+
+  it.only('Validate properties of the Contact Us Page', () => {
+    cy.visit('https://automationteststore.com/index.php?rt=content/contact');
+
+    //uses cypress commands and chaining
+    cy.contains('#ContactUsFrm', 'Contact Us Form')
+      .find('#field_11')
+      .should('contain', 'First name');
+
+    //JQuery approach
+
+    //embedded commands (Closure)
   });
 });
