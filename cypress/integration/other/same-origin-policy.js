cc7bf06ca7b55630149884fb9e87d86fa46db460
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-29 04:27:08 -0400

    same-origin-policy test

---
+++ cypress/integration/other/same-origin-policy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe('cypress web security', () => {
+  it.only('validate visiting two different domains', () => {
+    //this test will fail due to Cypress issue
+    cy.visit('http://www.webdriveruniversity.com/');
+    cy.visit('https://www.automationteststore.com/');
+  });
+  it.only('validate visiting two different domains via user scenarios', () => {
+    cy.visit('http://www.webdriveruniversity.com/');
+    cy.get('#automation-test-store').invoke('removeAttr', 'target').click();
+  });
+});
+///multiple superdomains in one test not supported (ticket open in Cypress)
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,7 +2,10 @@
 
 describe('Test Contact Us form via WebdriverUni', () => {
   it('Should be able to submit a successful submission via contact us form', () => {
-    cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    // cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+
+    cy.visit('http://www.webdriveruniversity.com/');
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
     cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
     cy.title().should('include', 'WebDriver | Contact Us');
     cy.url().should('include', 'contactus');
@@ -15,7 +18,10 @@ describe('Test Contact Us form via WebdriverUni', () => {
   });
 
   it('Should not be able to submit a successful submission via contact us form as all field are required', () => {
-    cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    // cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+
+    cy.visit('http://www.webdriveruniversity.com/');
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
     cy.get('[name="first_name"]').type('Mandi');
     cy.get('[name="last_name"]').type('Haase');
     cy.get('textarea.feedback-input').type('hello world');
